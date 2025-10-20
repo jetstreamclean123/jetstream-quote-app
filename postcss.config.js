59d57d536e51257445c83068da7e3811cdc850c9
jetstreamclean123 <jessfolmar39@gmail.com>
2025-10-19 17:52:28 -0700

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+/**
+ * PostCSS configuration for the Jet Stream Clean quote app.  This
+ * configuration wires up Tailwind CSS and Autoprefixer to the build
+ * pipeline used by Create React App.  You generally won't need to
+ * modify this file unless you add additional PostCSS plugins.
+ */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
